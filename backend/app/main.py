0f7ee4ca5e140b731c2db000f5b1b6294c7ac56b
Omar Watany <watany6500@gmail.com>
2026-05-11 08:07:43 +0300

    correct and better way to handle connection between front and back using vite proxy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.cv_matcher import router as cv_matcher_router
 from app.api.v1.db_info import router as db_info_router
 
 app = FastAPI(title="RAG CV Matching API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"], # Allow your frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cv_matcher_router, prefix="/api/v1")
 app.include_router(db_info_router, prefix="/api/v1/db", tags=["Database"])
